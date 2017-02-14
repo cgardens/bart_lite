cc02dfc0296de783ea2a723f7ff757cfd44babca
Charles Giardina <cgiardina@liveramp.com>
2017-02-13 23:07:02 -0800

    add a root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    render json: 'welcome to bart lite. for instructions on how to use bart lite, visit: https://github.com/cgardens/bart_lite'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', controller: :application, action: :index
+
   resources :arrivals, only: :index
   resources :valid_station_codes, only: :index
 
   get 'arrivals/go_to_work', controller: :arrivals, action: :go_to_work
   get 'arrivals/go_home', controller: :arrivals, action: :go_home
+
 end
